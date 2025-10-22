9a85e50c9f014498dfc7673443b8e98487ca4b99
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2025-10-22 11:40:33 -0400

    fixed nginx

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# Replace NEST_SERVER_PORT in nginx config
+sed -i "s/\${NEST_SERVER_PORT}/$NEST_SERVER_PORT/g" /etc/nginx/conf.d/default.conf
+
+# Start nginx
+nginx
+
 cd apps/server
 pnpm prisma migrate deploy
 pnpm prisma db seed
